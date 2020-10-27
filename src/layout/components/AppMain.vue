c31865595771d5a84844394ae9e7cff34de9c98d
[17676164797] <[604595092@qq.com]>
2020-10-27 18:47:07 +0800

    修复

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,41 +1,41 @@
 <template>
-  <main>
-    <section class="app-main">
-      <transition name="fade-transform" mode="out-in">
-        <router-view :key="key" />
-      </transition>
-    </section>
-  </main>
+    <main>
+        <section class="app-main">
+            <transition name="fade-transform" mode="out-in">
+                <router-view :key="key" />
+            </transition>
+        </section>
+    </main>
 </template>
 
 <script>
 export default {
-  name: "AppMain",
-  computed: {
-    key() {
-      return this.$route.path;
+    name: 'AppMain',
+    computed: {
+        key() {
+            return this.$route.path;
+        }
     }
-  }
 };
 </script>
 
 <style scoped lang="scss">
 .app-main {
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-  padding-bottom: 20px;
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+    padding-bottom: 20px;
 }
 
 .fixed-header + .app-main {
-  padding-top: 50px;
+    padding-top: 50px;
 }
 </style>
 
 <style lang="scss">
 .el-popup-parent--hidden {
-  .fixed-header {
-    padding-right: 15px;
-  }
+    .fixed-header {
+        padding-right: 15px;
+    }
 }
 </style>
